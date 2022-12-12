fea313f9980bc638e9256a6b762bce4fbceedbad
Ibrahim Saberi <georgeipsum@gmail.com>
2022-12-12 15:06:12 -0500

    reinit with tauri app

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-
-export default {
-  port: parseInt(process.env.PORT ?? "2345", 10),
-  spotifyClientId: process.env.SPOTIFY_CLIENT_ID ?? "",
-  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET ?? ""
-};
